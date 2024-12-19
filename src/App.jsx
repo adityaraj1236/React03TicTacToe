cce6e5c61c11a53dea034135c68806935cec0731
Your Name <adityaraj642003@gmail.com>
2024-12-19 22:15:04 +0530

    counter ap completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React , {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [count , setCount] = useState(0)
+  function decrementHandler() {
+    setCount(count-1)
+  }
+  function incrementHandler() {
+    setCount(count+1)
+  }
+  function resetHandler() {
+    setCount(0)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh] bg-[#344151] gap-10 select-none">
+        <div className="text-3xl font-semibold text-blue-300">Increment and Decrement</div>
+        <div className="flex text-blue-400 bg-white gap-8 px-6 py-4 rounded-xl text-2xl">
+          <button onClick={decrementHandler}>-</button>
+          <div className="border-r-2 border-l-2 py-2 px-4 font-bold">{count}</div>
+          <button onClick={incrementHandler}>+</button>
+        </div>
+        <div className="bg-blue-200 text-blue-700 px-4 py-3 rounded-xl hover:cursor-pointer select-none" onClick={resetHandler}>Reset</div>
+   </div>
   )
 }
 
